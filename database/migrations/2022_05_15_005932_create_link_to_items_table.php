79333e944d7219cf01d207a93cb37f5e51b20c44
Jesse Goble <jesse.goble@gmail.com>
2022-05-14 21:07:24 -0400

    Migrations created to build database.

---
+++ database/migrations/2022_05_15_005932_create_link_to_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinkToItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('link_to_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('link_to_items');
+    }
+}
